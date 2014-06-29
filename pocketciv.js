64bcc53ecdcf0989b607997669cf562073a6793c
Juho Rutila <juho.rutila@gmail.com>
2014-06-29 09:25:13 -0400

    Added map and tribemover

--- pocketciv.js
+++ pocketciv.js
@@ -39,6 +39,11 @@ var events = {
     },
 };
 
+var Set = function() {}
+Set.prototype.add = function(o) { this[o] = true; }
+Set.prototype.remove = function(o) { delete this[o]; }
+Set.prototype.values= function(o) { return Object.keys(this); }
+
 function NoMoreCardsError(message) {
 }
 
@@ -71,6 +76,107 @@ EventDeck.prototype = {
     },
 };
 
+function Map() {
+    this.areas = {}
+}
+
+Map.prototype = {
+    moveTribes: function(from, to, amount) {
+        this.areas[from].tribes -= amount;
+        this.areas[to].tribes += amount;
+    },
+    addTribe: function(area, amount) {
+        amount = typeof amount !== 'undefined' ? amount : 1;
+        this.areas[area].tribes += amount;
+    },
+    removeTribe: function(area, amount) {
+        amount = typeof amount !== 'undefined' ? amount : 1;
+        this.areas[area].tribes -= amount;
+    }
+}
+
+function TribeMover(map, moveLimit) {
+    this.start = {};
+    this.map = map;
+    this.neighbours = {};
+    this.neighbours2 = {};
+    for (var key in this.map)
+    {
+        var ngh = this.map[key]['neighbours'];
+        this.neighbours[key] = ngh.slice(0);
+        var ngh2 = ngh.slice(0);
+        for (var n in ngh2)
+        {
+            var n2 = this.map[ngh[n]]['neighbours'];
+            ngh2 = ngh2.concat(n2);
+        }
+        ngh2 = ngh2.filter(function (e, p) {
+            return ngh2.indexOf(e) == p;
+        })
+        ngh2.splice(ngh2.indexOf(parseInt(key)),1);
+        this.neighbours2[key] = ngh2;
+    }
+    this.moveLimit = moveLimit;
+}
+
+TribeMover.prototype = {
+    init: function(start) {
+        this.start = start;
+        this.max = this._nghValue(start);
+        this.ngh2 = this._2ngValue(start);
+    },
+    ok: function(situation) {
+        var ngh = this._nghValue(situation);
+        /*
+        console.log('--COMP--')
+        console.log('start')
+        console.log(this.start)
+        console.log('ngh')
+        console.log(this.neighbours)
+        console.log('ngh2')
+        console.log(this.neighbours2)
+        console.log('curr - situation')
+        console.log(situation)
+        console.log('curr - ngh')
+        console.log(ngh)
+        */
+        for (var key in situation)
+        {
+            var curr = situation[key];
+            if (curr > this.max[key]) return false;
+            if (ngh[key] < this.start[key]) return false;
+            if (ngh[key] > this.ngh2[key]) return false;
+        }
+        return true;
+    },
+    _nghValue: function(situation) {
+        var nghVal = {};
+        for (var key in situation)
+        {
+            var ngh = this.neighbours[key];
+            nghVal[key] = situation[key];
+            for (var n in ngh)
+            {
+                nghVal[key] += situation[ngh[n]];
+            }
+        }
+        return nghVal;
+    },
+    _2ngValue: function(situation) {
+        var nghVal = {};
+        for (var key in situation)
+        {
+            var ngh = this.neighbours2[key];
+            nghVal[key] = situation[key];
+            for (var n in ngh)
+            {
+                nghVal[key] += situation[ngh[n]];
+            }
+        }
+        return nghVal;
+    }
+}
+
 module.exports = {
     example: function(a) {
         return a + "--";
@@ -78,4 +184,6 @@ module.exports = {
     ,
     NoMoreCardsError: NoMoreCardsError,
     EventDeck: new EventDeck(),
+    Map: new Map(),
+    TribeMover: TribeMover,
 }
--- test/pocketciv.js
+++ test/pocketciv.js
@@ -43,3 +43,142 @@ describe('EventDeck', function() {
         });
     });
 });
+
+describe('Map', function() {
+    describe('tribes', function() {
+        describe('movement', function() {
+            beforeEach(function() {
+                pocketciv.Map.areas = {
+                        1: {
+                            'tribes': 2,
+                            'neighbours': [2]
+                        },
+                        2: {
+                            'tribes': 2,
+                            'neighbours': [1,3]
+                        },
+                        3: {
+                            'tribes': 2,
+                            'neighbours': [2]
+                        }
+                };
+            });
+            it('should move tribes from area to another', function() {
+                pocketciv.Map.moveTribes(1,2,1);
+                pocketciv.Map.areas[1].tribes.should.equal(1);
+                pocketciv.Map.areas[2].tribes.should.equal(3);
+            });
+        });
+        describe('changes', function() {
+            beforeEach(function() {
+                pocketciv.Map.areas = {
+                        1: {
+                            'tribes': 2,
+                        },
+                };
+            });
+            it('should remove a tribe from area', function() {
+                pocketciv.Map.removeTribe(1);
+                pocketciv.Map.areas[1].tribes.should.equal(1);
+            });
+            it('should add a tribe to area', function() {
+                pocketciv.Map.addTribe(1); // Area: 1
+                pocketciv.Map.areas[1].tribes.should.equal(3);
+            });
+            it('should remove multiple tribes from area', function() {
+                pocketciv.Map.removeTribe(1, 2);
+                pocketciv.Map.areas[1].tribes.should.equal(0);
+            });
+            it('should add multiple tribes to area', function() {
+                pocketciv.Map.addTribe(1, 5); // Area: 1
+                pocketciv.Map.areas[1].tribes.should.equal(7);
+            });
+        })
+    });
+});
+
+describe('TribeMover', function() {
+    describe('simple', function() {
+        beforeEach(function() {
+            map = {
+                1: { 'neighbours': [2] },
+                2: { 'neighbours': [1,3] },
+                3: { 'neighbours': [2] },
+            }
+            mover = new pocketciv.TribeMover(map, 1);
+        });
+        it('case 1', function() {
+            mover.init({ 1: 1, 2: 1, 3: 1 });
+            mover.ok({ 1: 1, 2: 1, 3: 1 }).should.be.true;
+        });
+        it('case 2', function() {
+            mover.init({ 1: 1, 2: 1, 3: 0 });
+            mover.ok({ 1: 0, 2: 1, 3: 1 }).should.be.true;
+        });
+        it('case 3', function() {
+            mover.init({ 1: 2, 2: 0, 3: 0 });
+            mover.ok({ 1: 0, 2: 2, 3: 0 }).should.be.true;
+            mover.ok({ 1: 1, 2: 1, 3: 0 }).should.be.true;
+            mover.ok({ 1: 1, 2: 0, 3: 1 }).should.be.false;
+        });
+        it('case 4 - neighbour limit', function() {
+            mover.init({ 1: 2, 2: 0, 3: 0 });
+            mover.ok({ 1: 2, 2: 2, 3: 0 }).should.be.false;
+            mover.ok({ 1: 0, 2: 0, 3: 0 }).should.be.false;
+        });
+    });
+    describe('complex', function() {
+        beforeEach(function() {
+            //   1 - 2
+            //        \
+            //         3
+            //        / \
+            //       4 - 5
+            map = {
+                1: { 'neighbours': [2] },
+                2: { 'neighbours': [1,3] },
+                3: { 'neighbours': [2,4,5] },
+                4: { 'neighbours': [3,5] },
+                5: { 'neighbours': [3,4] },
+            }
+            mover = new pocketciv.TribeMover(map, 1);
+        });
+        it('case 1', function() {
+            mover.init({ 1: 1, 2: 1, 3: 1, 4: 0, 5: 0 });
+            mover.ok({ 1: 1, 2: 1, 3: 1, 4: 0, 5: 0 }).should.be.true;
+            mover.ok({ 1: 0, 2: 1, 3: 1, 4: 1, 5: 0 }).should.be.true;
+            mover.ok({ 1: 0, 2: 2, 3: 1, 4: 0, 5: 0 }).should.be.true;
+            mover.ok({ 1: 0, 2: 3, 3: 0, 4: 0, 5: 0 }).should.be.true;
+        });
+        it('case 2', function() {
+            mover.init({ 1: 0, 2: 0, 3: 0, 4: 1, 5: 1 });
+            mover.ok({ 1: 0, 2: 0, 3: 2, 4: 0, 5: 0 }).should.be.true;
+            mover.ok({ 1: 0, 2: 0, 3: 1, 4: 1, 5: 0 }).should.be.true;
+            mover.ok({ 1: 0, 2: 0, 3: 1, 4: 0, 5: 1 }).should.be.true;
+            mover.ok({ 1: 0, 2: 0, 3: 0, 4: 0, 5: 2 }).should.be.true;
+        });
+    });
+    describe('big one', function() {
+        beforeEach(function() {
+            //       2 - 7
+            //      / \ / \
+            //     1 - 3 - 6
+            //      \ / \ /
+            //       4 - 5
+            map = {
+                1: { 'neighbours': [2, 3, 4] },
+                2: { 'neighbours': [1, 3, 7] },
+                3: { 'neighbours': [1, 2, 7, 6, 5, 4] },
+                4: { 'neighbours': [1, 3, 5] },
+                5: { 'neighbours': [3, 4, 6] },
+                6: { 'neighbours': [3, 5, 7] },
+                7: { 'neighbours': [2, 3, 6] },
+            }
+            mover = new pocketciv.TribeMover(map, 1);
+        });
+        it('case 1', function() {
+            mover.init({ 1: 0, 2: 0, 3: 6, 4: 0, 5: 0, 6: 0, 7: 0 });
+            mover.ok({ 1: 1, 2: 1, 3: 0, 4: 1, 5: 1, 6: 1, 7: 1 }).should.be.true;
+        });
+    });
+});
