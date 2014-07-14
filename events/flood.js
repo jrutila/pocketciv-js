1a0f1943d8964e8f1b38ecf79d41f648c4c25abd
Juho Rutila <juho.rutila@gmail.com>
2014-07-14 08:05:23 -0400

    Flood fixed

--- events/flood.js
+++ events/flood.js
@@ -4,24 +4,20 @@ module.exports = {
     punchline: 'Your floor is quite wet...',
     description: "",
     steps: {
-            '1': "{% area_card %}",
-            '2.1': "If the {{ Active Region|area }} neighbors the Sea then see TSUNAMI below",
+            '1': "{%; area_card() %}",
+            '2.1': "If the {{ Active Region|active_region}} neighbors the Sea then see TSUNAMI below",
             '2.2.1': "If the Active Region does not Neighbor the Sea: \
                     Reduce Tribes by 2 in the Active Region. \
                     Decimate Farms in the Active Region. \
                     Reduce City AV by 1 in the Active Region. \
                     Create a Forest in the Active Region.\
-                    {% if (!('sea' in area.neighbours )) { %} \
-                    {% change = {'tribes': '-2', 'farm': false, 'city': '-1', 'forest': true } %} \
-                    {% } %}",
-            '3': "TSUNAMI {% break_if(!('sea' in area.neighbours )) %}",
-            '3.1': "Draw the next Event Card {% draw_card() %}. Using the symbols shown \
+                    {% change({'tribes': '-2', 'farm': false, 'city': '-1', 'forest': true }) %}",
+            '3': "TSUNAMI {% break_if(active_region.neighbours.indexOf('sea') == -1) %}",
+            '3.1': "Draw the next Event Card {%; draw_card() %}. Using the symbols shown \
                     on the ORIGINAL Event card to the far right of the \
                     Flood event, add up the values in the symbols on the \
                     newly drawn card. The total value that this creates \
-                    determines Damage. \
-                    {% change = undefined %} \
-                    {% damage = card_value(event.expr) %}",
+                    determines Damage.",
             '3.2': "Each Region Neighboring the Sea (that neighbors \
 the Active Region) is inflicted with this amount of \
 Damage, and must have the following elements \
@@ -30,24 +26,36 @@ first, then any remaining Damage after all Tribes are \
 Reduced is inflicted on City AV. If a City is \
 Decimated, then remaining damage is inflicted on \
 Wonders of your choice in the Region, one at a time. \
+                    {% changes = {} %} \
+                    {% damage = card_value(event.expr) %} \
  \
 1 Tribe = 1 Damage. {% dmgTr = 1 %} \
 1 City AV = 2 Damage. {% dmgCt = 2 %} \
 1 Wonder = 3 Damage. {% dmgWn = 3 %}",
-            '-': "{% tsunami %}"
+            '-': "{% tsunami() %}"
     },
     'tsunami': function() {
         console.log("Running TSUNAMI!")
-        var ngh = neighbours(area);
-        /*
-            '-': "{% ngh = neighbours(area) %} \
-                  {% for (var a in ngh) { %} \
-                  {%  if ('sea' in ngh[a].neighbours) { %} \
-                  {%   var tribes = Math.min(damage, dmgTr*ngh[a].tribes) %}
-                  {%   var city = Math.min()
-                  {%   changes[a]
-                  {% }} %}
-                  "
-                  */
+        var ngh = this.active_region.neighbours.concat([this.active_region.id])
+        for (var a in ngh)
+        {
+            var area = this.engine.map.areas[ngh[a]];
+            if (area && area.neighbours.indexOf('sea') > -1)
+            {
+                var dmg = damage;
+                var tribes = Math.min(Math.ceil(dmg/dmgTr), Math.ceil(area.tribes/dmgTr))
+                dmg -= tribes*dmgTr;
+                //console.log(area.id+ ' ' +Math.ceil(dmg/dmgCt) +' '+ Math.ceil(area.city/dmgCt))
+                var city = Math.min(Math.ceil(dmg/dmgCt), Math.ceil(area.city/dmgCt))
+                dmg -= tribes*dmgCt;
+                // TODO: Wonders!
+                if (tribes || city)
+                {
+                    this.changes[area.id] = {}
+                    if (tribes) this.changes[area.id].tribes = (area.tribes-tribes).toString();
+                    if (city) this.changes[area.id].city = (area.city-city).toString();
+                }
+            }
+        }
     },
 }
--- test/events.js
+++ test/events.js
@@ -1,4 +1,5 @@
 var should = require('chai').should()
+require('mocha')
 var eventRunner = require('../core/event')
 var pocketciv = require('../core/pocketciv')
 
@@ -18,7 +19,7 @@ describe('Famine', function() {
     });
 });
 
-describe('Flood', function() {
+describe.only('Flood', function() {
     beforeEach(function() {
         runEvent = eventRunner.runEvent;
         event = require('../events/flood')
@@ -29,13 +30,60 @@ describe('Flood', function() {
             3: { id: 3, neighbours: [ 4, 2 ] },
             2: { id: 2, neighbours: [ 3, 'sea' ] },
         }
-        engine.drawer = function(deck, done) { done({ 'circle': 5}) }
     })
     it('should be a flood if not next to sea', function(done) {
+        engine.drawer = function(deck, done) { done({ 'circle': 3}) }
         runEvent(engine, event, {}, function(chg) {
-            chg.should.deep.equal({ 5: {'tribes': '0', 'farm': false, 'city': '-2'}})
+            chg.should.deep.equal({ 3: {'tribes': '-2', 'farm': false, 'city': '-1', 'forest': true}})
             done();
         })
     });
-    
+    it('should be a TSUNAMI if next to sea', function(done) {
+        engine.map.areas[5].tribes = 4;
+        engine.map.areas[5].city = 4;
+        engine.map.areas[4].tribes = 5;
+        engine.map.areas[4].city = 5;
+        
+        var deckk = [{ circle: 5 }, { square: 7 }]
+        engine.drawer = function(deck, done) { done(deckk.shift()) }
+        runEvent(engine, event, { expr: 's' }, function(chg) {
+            chg.should.deep.equal({
+                5: {'tribes': '0', 'city': '2' },
+                4: {'tribes': '0', 'city': '4' },
+            })
+            done();
+        })
+    });
+    it('tsunami case 1', function(done) {
+        engine.map.areas[5].tribes = 4;
+        engine.map.areas[5].city = 4;
+        engine.map.areas[4].tribes = 5;
+        engine.map.areas[4].city = 5;
+        
+        var deckk = [{ circle: 5 }, { square: 4 }]
+        engine.drawer = function(deck, done) { done(deckk.shift()) }
+        runEvent(engine, event, { expr: 's' }, function(chg) {
+            chg.should.deep.equal({
+                5: {'tribes': '0' },
+                4: {'tribes': '1' },
+            })
+            done();
+        })
+    });
+    it('tsunami case 2', function(done) {
+        engine.map.areas[5].tribes = 4;
+        engine.map.areas[5].city = 4;
+        engine.map.areas[4].tribes = 0;
+        engine.map.areas[4].city = 5;
+        
+        var deckk = [{ circle: 5 }, { square: 5 }]
+        engine.drawer = function(deck, done) { done(deckk.shift()) }
+        runEvent(engine, event, { expr: 's' }, function(chg) {
+            chg.should.deep.equal({
+                5: {'tribes': '0', 'city': '3' },
+                4: {'city': '2' },
+            })
+            done();
+        })
+    });
 })
