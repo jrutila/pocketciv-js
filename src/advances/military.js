ea6a7f9d8d487a6d664d35baaab27594980beb74
Juho Rutila <juho.rutila@gmail.com>
2015-02-24 20:37:23 +0000

    Uprising and Military (fixes #4)

--- src/advances/military.js
+++ src/advances/military.js
@@ -1,3 +1,5 @@
+var reducer = require("../core/reducer");
+
 module.exports = {
     name: "military",
     title: "Military",
@@ -24,6 +26,45 @@ module.exports = {
                         {% neighbourCityReduce = -3 %}"
             }
         },
+        'uprising': {
+            'steps': {
+                '4': "- If you have {{ adv:military }}, Select a Neighboring \
+Region with a City. Reduce Neighboring City \
+AV by 1. Decimate Tribes in selected \
+Neighboring Region, unless you have \
+Organized Religion, then reduce Neighboring Tribes by 2.\
+{%; reduceNeighbour() %}"
+            },
+            reduceNeighbour: function() {
+                var ctx = this;
+                var engine = this.engine;
+                var initial = _.pick(engine.map.areas, function(area, ak) {
+                    ak = parseInt(ak);
+                    return (_.contains(this.active_region.neighbours, ak) &&
+                        area.city > 0);
+                        
+                },this);
+                var opts = reducer.Templates.basic(ctx, ['city']);
+                opts.initial = initial;
+                opts.tribeChange = tribeChange;
+                opts.amount = 1;
+                opts.edits = [];
+                opts.shows = ['city', 'tribes'];
+                opts.reduce = function(key) {
+                    this.amount--;
+                    var ret = { 'city': this.initial[key].city-1 };
+                    if (this.opts.tribeChange == "0")
+                        ret.tribes = 0;
+                    else
+                        ret.tribes = this.initial[key].tribes + parseInt(this.opts.tribeChange);
+                    return ret;
+                }
+                engine.reducer(new reducer.Reducer(opts), function(chg) {
+                    ctx.merge(chg);
+                    ctx.done && ctx.done();
+                });
+            }
+        }
     },
     acquired: function() { },
     phases: { },
