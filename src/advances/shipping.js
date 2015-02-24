98bc03767dc2f35a2af42dec04cfa9e44f73e9cb
Juho Rutila <juho.rutila@gmail.com>
2015-02-24 13:04:55 +0000

    Trade and Shipping fixes #6

--- src/advances/shipping.js
+++ src/advances/shipping.js
@@ -9,7 +9,23 @@ module.exports = {
     events: {
         'trade': {
             'steps': {
-                // TODO:
+                '2': "+ If you have {{ adv:shipping }}, and the card that you \
+                    have just drawn to indicate your Gold income \
+                    also has a HANDSHAKE, draw another Event \
+                    card. Increase the amount of Gold in your \
+                    common stock by the value in the GREEN \
+                    SQUARE on this newly drawn card.{%; drawMore() %}"
+            },
+            drawMore: function() {
+                var ctx = this;
+                if (ctx.card.friendly)
+                {
+                    ctx.engine.draw(function(c) {
+                        ctx.merge({ 'gold': '+'+c.square });
+                        ctx.done && ctx.done();
+                    });
+                } else 
+                    ctx.done && ctx.done();
             }
         }
     },
