bd15989b8173c691c17a28d87a15e49dd30a4ec3
Juho Rutila <juho.rutila@gmail.com>
2015-02-27 12:43:29 +0000

    Refactore trade

--- src/events/trade.js
+++ src/events/trade.js
@@ -5,6 +5,6 @@ module.exports = {
     title: 'Trade',
     steps: {
         '1': "Draw the next Event card {%; draw_card() %}. Increase the amount of \
-              Gold you currently have by the value in the RED CIRCLE. {% change({'gold': '+'+card_value('c') }, null) %} ",
+              Gold you currently have by the value in the RED CIRCLE. {% change('gold', card_value('c')) %} ",
     },
 };
