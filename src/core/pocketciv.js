7cac16e4bf27c57d0a0e701801c5150becc350d0
Juho Rutila <juho.rutila@gmail.com>
2015-03-19 05:21:35 +0000

    Change CiytCount to CityAV

--- src/core/pocketciv.js
+++ src/core/pocketciv.js
@@ -166,8 +166,11 @@ Map.prototype = {
     get tribeCount() {
       return _.reduce(_.values(this.areas), function(memo, area){ return area.tribes ?  memo + area.tribes: memo }, 0)
     },
-    get cityCount() {
+    get cityAV() {
       return _.reduce(_.values(this.areas), function(memo, area){ return area.city ?  memo + area.city: memo }, 0)
+    },
+    get cityCount() {
+      return _.reduce(_.values(this.areas), function(memo, area){ return area.city ?  memo + 1: memo }, 0)
     },
 }
 
